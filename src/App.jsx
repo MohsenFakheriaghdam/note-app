6c5723394c0de51de6e6325615d3a8504fddb51f
Mohsen <mohsen.6ix.ca@gmail.com>
2026-08-09 14:36:28 +0330

    develop note complete section

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,17 @@ export default function App() {
 	const [notes, setNotes] = useState([]);
 	const handleAddNewNotes = (newNote) => setNotes((prevNotes) => [...prevNotes, newNote]);
 	const handleDeleteNote = (id) => setNotes((prevNotes) => prevNotes.filter((n) => n.id !== id));
+	const handleCheckNote = (e) => {
+		const noteId = e.target.value;
+		setNotes((prevNotes) => prevNotes.map((note) => (note.id === noteId ? { ...note, isCompleted: !note.isCompleted } : note)));
+	};
 
 	return (
 		<div className="app_container h-screen grid grid-cols-1 grid-rows-[auto_1fr]">
 			<Header />
 			<div className="grid grid-rows-1 grid-cols-[2fr_4fr] mt-4 gap-10">
 				<AddNewNote onAddNote={handleAddNewNotes} />
-				<NoteList notes={notes} onDelete={handleDeleteNote} />
+				<NoteList notes={notes} onDelete={handleDeleteNote} checkNote={handleCheckNote} />
 			</div>
 			<Footer />
 		</div>
--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -1,10 +1,10 @@
-export default function NoteList({ notes, onDelete }) {
+export default function NoteList({ notes, onDelete, checkNote }) {
 	return (
 		<div className="">
 			{/* filter notes */}
 			<FilterNotes />
 			{/* Notes */}
-			<Notes notes={notes} key={notes.id} onDelete={onDelete} />
+			<Notes notes={notes} key={notes.id} onDelete={onDelete} checkNote={checkNote} />
 		</div>
 	);
 }
@@ -30,24 +30,24 @@ function FilterNotes() {
 	);
 }
 
-function Notes({ notes, onDelete }) {
+function Notes({ notes, onDelete, checkNote }) {
 	return (
 		<div className="flex flex-col gap-5 mt-7">
 			{/* Note */}
 			{notes.map((note) => {
 				return (
-					<div className="flex flex-col gap-3 bg-white p-4 rounded-xl" key={note.id}>
+					<div className={`flex flex-col gap-3 bg-white p-4 rounded-xl ${note.isCompleted ? "text-gray-500" : ""}`} key={note.id}>
 						{/* note-header */}
 						<div className=" flex items-center justify-between">
 							<div className="flex flex-col gap-1">
-								<h2 className="font-bold text-xl">{note.title}</h2>
+								<h2 className={`font-bold text-xl ${note.isCompleted ? "line-through" : ""}`}>{note.title}</h2>
 								<p className="text-gray-500 font-medium">{note.description}</p>
 							</div>
 							<div className="flex gap-4 ">
 								<button className="cursor-pointer" onClick={() => onDelete(note.id)}>
 									❌
 								</button>
-								<input type="checkbox" className="size-6" />
+								<input type="checkbox" className="size-6" value={note.id} name={note.id} id={note.id} checked={note.isCompleted} onChange={checkNote} />
 							</div>
 						</div>
 						{/* line */}
